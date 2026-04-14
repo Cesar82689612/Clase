a08f3385372d78ddb7c1b3dcd4b5f9298e55de17
Cesar82689612 <cesardelafcegax09@gmail.com>
2026-04-14 13:30:09 -0600

    Add basic calculator functionality in C

---
+++ calculadora.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+  float n1,n2,res;
+  int op=0;
+  printf("Bienvenida a mi calculadora\n");
+  printf("Escoge una opción\n");
+  printf("1.-Suma\n");
+  printf("2.-Resta\n");
+  printf("3.-Multiplicación\n");
+  printf("4.-Division\n");
+  printf("opcion: ");
+  scanf("%d",&op);
+  printf("Ingresa dos cantidades separadas por espcio \n");
+  scanf("%f %f",&n1,&n2);
+  switch(op)
+  {
+    Default:
+      printf("Opción no valida");
+    case 1:
+      res=n1+n2;
+      printf("El resultado es %f\n",res);
+      break;
+    case 2:
+      res=n1-n2;
+      printf("El resultado es %f\n",res);
+      break;
+    case 3:
+      res=n1*n2;
+      printf("El resultado es %f\n",res);
+      break;    
+    case 4:
+      res=n1/n2;
+      printf("El resultado es %f\n",res);
+      break;
+  }
+  return 0;
+}
